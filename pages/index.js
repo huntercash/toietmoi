abf2dbcee6aabc14cff3404f4fbd4756ea2da348
Hunter Cash <hmclaug@gmail.com>
2020-07-21 18:46:00 -0500

    the meat

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,32 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import countdown from 'countdown';
+// The day we meet
+const date = new Date('2020-08-12T17:40:00Z');
+// The variable that looks nice
+const count = countdown(date);
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Toi et Moi</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <h1>{`${count.days} Days`}</h1>
+        </div>
+        <div>
+          <h2>{`${count.hours} Hours`}</h2>
+        </div>
+        <div>
+          <h3>{`${count.minutes} Minutes`}</h3>
+        </div>
+        <div>
+          <h4>{`${count.seconds} Seconds`}</h4>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -205,5 +174,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
